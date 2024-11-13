52a3a2c4fadea0f9b113823f54d068ce4e0db526
Wyatt Smith <wyattjorgen@gmail.com>
2024-11-13 12:44:15 -0800

    Add project files.

---
+++ WyattProgrammingTestSite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WyattProgrammingTestSite.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
